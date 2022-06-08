91ce2e2d8679c56b3a3d734a0a5f164673bb3b72
Ductc2410 <ductc1@vmodev.com>
2022-06-09 00:42:19 +0700

    Init Project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>lorem</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function button(props) {
+  return <div>button</div>;
+}
+
+button.propTypes = {};
+
+export default button;
